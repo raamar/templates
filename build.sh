1274873e8f480caee981a86c55a30b5c7a1c861e
raamar <khandryga.romil@gmail.com>
2021-05-21 18:23:05 +0300

    new build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 build() {
-  rm -rf ./build/*.o
   mkdir -p build
-  bPath=./build/$1.exe
-
-  for f in ./$1.cpp; do
-    g++ -c $f -o build/"${f##*/}".o
-  done
-
-  g++ -o $bPath build/*.o
-  $bPath
+  cd build/
+  
+  g++ -c ../src/*.cpp
+  g++ -c ../$1.cpp
+  g++ -o $1.exe *.o
+  
+  ./$1.exe
+  
+  rm -rf ./*.o
+  mv $1.exe $1.exe.old
 }
 
 build $1
