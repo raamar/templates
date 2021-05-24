02641fb319dd9a722a7e3d8413e86a273931fe44
raamar <khandryga.romil@gmail.com>
2021-05-24 13:52:05 +0300

    build.sh

--- build.sh
+++ build.sh
@@ -2,10 +2,9 @@ build() {
   mkdir -p build
   cd build/
   
-  [ -f ../src/*.cpp ] && g++ -c ../src/*.cpp
-  [ -f ../$1.cpp ]    && g++ -c ../$1.cpp
-  [ -f *.o ]          && g++ -o $1.exe *.o
-  [ -f ./$1.exe ]     && { ./$1.exe; mv $1.exe $1.exe.old; }
+  g++ -c ../$1.cpp
+  g++ -o $1.exe *.o
+  [ -f ./$1.exe ] && { ./$1.exe; mv $1.exe $1.exe.old; }
   
   rm -rf ./*.o
 }
